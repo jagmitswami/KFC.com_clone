e214cbc017e48148b45c491561c1a2cfee8a1459
Ajay11125 <yadavajay11125@gmail.com>
2022-08-25 17:57:51 +0530

    responsive navbar

---
+++ scripts/navbar.js
@@ -0,0 +1,14 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "100%";
+    document.getElementById("burger").style.marginLeft = "250px";
+    document.getElementById("mySidebar").style.display = "block";
+    
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("burger").style.marginLeft= "0";
+    document.getElementById("mySidebar").style.display = "none";
+
+  }
+
