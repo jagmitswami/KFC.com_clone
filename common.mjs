6c9d3273e31382858ad85e3bcbe4483e1363f7c9
Shreedhar Hegde <snhkelemane@gmail.com>
2022-08-26 23:35:06 +0530

    Made Cart page responsive and added add more and remove item functionalities

--- common.mjs
+++ common.mjs
@@ -8,3 +8,22 @@ export function getCart() {
 export function setCart(value) {
 	return localStorage.setItem("cart", JSON.stringify(value));
 }
+
+/* Add to cart functionality */
+
+export function addItemTocart(id, el) {
+	let cart = getCart();
+	let itemExist = cart.filter((menu) => menu.id === id);
+	console.log(itemExist);
+
+	itemExist.length > 0
+		? (cart = cart.map((menu) => {
+				menu = menu.id === id ? { ...menu, count: menu.count + 1 } : menu;
+				return menu;
+		  }))
+		: (cart = [...cart, { ...el, count: 1 }]);
+
+	console.log(cart);
+	setCart(cart);
+  
+}
