7ab254774779f606983f3b5a3912cf48f825de19
Shreedhar Hegde <snhkelemane@gmail.com>
2022-10-09 01:08:41 +0530

    Updated footer

--- components/footer.js
+++ components/footer.js
@@ -1,54 +1,140 @@
 function getFooter() {
   return `  
-    <div>
+  <footer>
+  <div class="footer-main">
+    <div class="logo-container">
       <img
         src="https://images.ctfassets.net/wtodlh47qxpt/25FSYFuEtGct8NSrtpKe6d/b602f6fe0bf294e6a6dff5d7648bf594/KFC_Logo.svg"
-        alt="">
+        alt=""
+      />
     </div>
-    <div></div>
-    <div>
-      <a>KFC Food</a>
-      <a>Menu</a>
-      <a>Order Lookup</a>
-      <a>Gift Card</a>
-      <a>Nutrition & Allergen</a>
+    <div class="link-container">
+      <div>
+        <a href=""> KFC Food </a>
+        <i class="fa-light fa-chevron-down"></i>
+      </div>
+      <div>
+        <a href="menu.html"> Menu </a>
+      </div>
+      <div>
+        <a href=""> Order Lookup </a>
+      </div>
+      <div>
+        <a href=""> Gift Card </a>
+      </div>
+      <div>
+        <a href=""> Nutrition & Allergen </a>
+      </div>
     </div>
-    <div>
-      <a>Support</a>
-      <a>Get Help</a>
-      <a>Contact Us</a>
-      <a>KFC Feedback</a>
-      <a>Privacy Policy</a>
+    <div class="link-container">
+      <div>
+        <a href=""> Support </a>
+        <i class="fa-light fa-chevron-down"></i>
+      </div>
+      <div>
+        <a href=""> Get Help </a>
+      </div>
+      <div>
+        <a href=""> Contact Us </a>
+      </div>
+      <div>
+        <a href=""> KFC Feedback </a>
+      </div>
+      <div>
+        <a href=""> Privacy Policy </a>
+      </div>
     </div>
-    <div>
-      <a>Legal</a>
-      <a>Terms and Conditions</a>
-      <a>Privacy Policy</a>
-      <a>Disclaimer</a>
-      <a>Caution Notice</a>
+    <div class="link-container">
+      <div>
+        <a href=""> Legal </a>
+        <i class="fa-light fa-chevron-down"></i>
+      </div>
+      <div>
+        <a href=""> Terms and Conditions </a>
+      </div>
+      <div>
+        <a href=""> Privacy Policy </a>
+      </div>
+      <div>
+        <a href=""> Disclaimer </a>
+      </div>
+      <div>
+        <a href=""> Caution Notice </a>
+      </div>
     </div>
-    <div>
-      <a>KFC India</a>
-      <a>About KFC</a>
-      <a>KFC Care</a>
-      <a>Careers</a>
-      <a>Our Golden Past</a>
+    <div class="link-container">
+      <div>
+        <a href=""> KFC India </a>
+        <i class="fa-light fa-chevron-down"></i>
+      </div>
+      <div>
+        <a href=""> About KFC </a>
+      </div>
+      <div>
+        <a href=""> KFC Care </a>
+      </div>
+      <div>
+        <a href=""> Careers </a>
+      </div>
+      <div>
+        <a href=""> Our Golden Past </a>
+      </div>
     </div>
     <div class="loc">
-      <div><i class="fa-solid fa-location-dot"></i></div><a>Find a KFC</a>
-
+      <div>
+        <img
+          src="https://images.ctfassets.net/wtodlh47qxpt/6qgKpWUOIsrIiazhk3cdmF/d60b4c20be69bab1f939bf33348b67e9/Find_KFC.svg"
+          alt="locate_us"
+        />
+      </div>
+      <div>
+        <a>Find a KFC</a>
+      </div>
     </div>
-    <div></div>
+    <div class="app-download">
+      <p>Download app</p>
+      <div class="app-links">
+        <div>
+          <img
+            src="https://images.ctfassets.net/wtodlh47qxpt/6BdZsyjLn64c06uCIE73d1/fb530f5d5231533b049463f6c7e8a2b1/google_play.svg"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            src="https://images.ctfassets.net/wtodlh47qxpt/em3mcMuAdXWlgucSJiTbS/d3ae7e51ed101d829e459355e255c47f/apple.svg"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="bottom-links">
+    <div class="social-logo">
+      <div>
+        <img
+          src="https://images.ctfassets.net/wtodlh47qxpt/4ZHyPA2EeaoP3aqtNDriBA/463462a9c27b0aa585e12b21ce3766e0/Social_Insta_White.svg"
+          alt="instagram_link"
+        />
+      </div>
+      <div>
+        <img
+          src="https://images.ctfassets.net/wtodlh47qxpt/dKiu2meLcfz2DNwsg7nZw/7194830b1ba6f25b158a23d6b2c4752c/Social_Facebook_White.svg"
+          alt="facebook_link"
+        />
+      </div>
+      <div>
+        <img
+          src="https://images.ctfassets.net/wtodlh47qxpt/78z9x0WwdkdXwGVK726EKX/6599ca34ec88e2a6f46d7d94ed85a8ad/Social_Twitter_White.svg"
+          alt="twitter_link"
+        />
+      </div>
+    </div>
     <div>
-      <img
-        src="https://images.ctfassets.net/wtodlh47qxpt/6BdZsyjLn64c06uCIE73d1/fb530f5d5231533b049463f6c7e8a2b1/google_play.svg"
-        alt="">
+      <p>Copyright © KFC Corporation 2021 All Rights Reserved</p>
     </div>
-    <div>
-      <img
-        src="https://images.ctfassets.net/wtodlh47qxpt/em3mcMuAdXWlgucSJiTbS/d3ae7e51ed101d829e459355e255c47f/apple.svg"
-        alt="">
-    </div>
+  </div>
+</footer>
    `;
 }
 
