e89523af306c7a382f6fd7be93fab87d7b285cb2
Shreedhar Hegde <snhkelemane@gmail.com>
2022-10-09 01:07:51 +0530

    removed unused log statement

--- components/cart.mjs
+++ components/cart.mjs
@@ -1,29 +1,29 @@
 /* Getting items in the cart */
 export function getCart() {
-	return JSON.parse(localStorage.getItem("cart")) || [];
+  return JSON.parse(localStorage.getItem("cart")) || [];
 }
 
 /* Adding items to the cart */
 
 export function setCart(value) {
-	return localStorage.setItem("cart", JSON.stringify(value));
+  return localStorage.setItem("cart", JSON.stringify(value));
 }
 
 /* Add to cart functionality */
 
 export function addItemTocart(id, el) {
-	let cart = getCart();
-	let itemExist = cart.filter((menu) => menu.id === id);
-	console.log(itemExist);
+  let cart = getCart();
+  let itemExist = cart.filter((menu) => menu.id === id);
+  // console.log(itemExist);
 
-	itemExist.length > 0
-		? (cart = cart.map((menu) => {
-				menu = menu.id === id ? { ...menu, count: menu.count + 1 } : menu;
-				return menu;
-		  }))
-		: (cart = [...cart, { ...el, count: 1 }]);
+  itemExist.length > 0
+    ? (cart = cart.map((menu) => {
+        menu = menu.id === id ? { ...menu, count: menu.count + 1 } : menu;
+        return menu;
+      }))
+    : (cart = [...cart, { ...el, count: 1 }]);
 
-	console.log(cart);
-	setCart(cart);
-    getcartdata()
+  // console.log(cart);
+  setCart(cart);
+  getcartdata();
 }
